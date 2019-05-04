6cbdc71102c3fceaaa1ae16e6a1e23d2907708b1
Asilbek5999 <asilbek.aliev.1999@mail.ru>
2019-05-04 17:35:19 +0500

    project initialized, models, migrations

---
+++ database/migrations/2019_05_04_114643_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->string('address');
+            $table->string('phone_number');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders', function ($table){
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['product_id']);
+        });
+    }
+}
